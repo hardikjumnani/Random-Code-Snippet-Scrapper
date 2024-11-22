24216b0e39ec9a1e6326c15aba08339f17803bf1
Hardik Jumnani <hardik.jumnani123@gmail.com>
2024-11-22 23:36:56 +0530

    populate solutions db with code

--- main.py
+++ main.py
@@ -2,9 +2,12 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
 
+from bs4 import BeautifulSoup, element
+
 import sqlite3
 
 import random
+
 
 # Initialize the Selenium WebDriver (e.g., for Chrome)
 options = webdriver.ChromeOptions()
@@ -12,6 +15,7 @@ options.headless = True  # Run in headless mode if you don't want a visible brow
 driver = webdriver.Chrome(options=options)
 
 URL = {
+    'leetcode': f'https://leetcode.com',
     'problemset': f'https://leetcode.com/problemset',
     'problems': f'https://leetcode.com/problems'
 }
@@ -26,5 +30,22 @@ rand_row_idx = random.randint(0, row_count-1)
 cursor.execute('SELECT * FROM problems LIMIT 1 OFFSET ?', (rand_row_idx, ))
 row = cursor.fetchone()
 
-driver.get(URL['problems'] + '/' + row[2])
+driver.get(f'{URL["problems"]}/{row[2]}/solutions')
 time.sleep(5)
+
+soup = BeautifulSoup(driver.page_source, 'html.parser')
+
+solutions_divs = soup.find_all('div', class_=['flex', 'w-full', 'flex-col'])
+solutions_divs = [div for div in solutions_divs if set(div['class']) == {'flex', 'w-full', 'flex-col'}]
+# for i in range(len(solution_divs)): print(solution_divs[i], '\n')
+
+solution_link = solutions_divs[0].find_next('div').find_all('a')[-1].get('href')
+# print(solution_link)
+
+driver.get(f'{URL["leetcode"]}{solution_link}')
+time.sleep(5)
+
+soup = BeautifulSoup(driver.page_source, 'html.parser')
+# print(soup.prettify())
+print(soup.find('pre').find('code'))
+# for i in soup.find('pre').find('code'): ... # Code to tag converter
---
+++ populate_solutions_db.py
@@ -0,0 +1,87 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+from bs4 import BeautifulSoup, element
+
+import sqlite3
+
+# Initialize the Selenium WebDriver (e.g., for Chrome)
+options = webdriver.ChromeOptions()
+options.headless = True  # Run in headless mode if you don't want a visible browser window
+driver = webdriver.Chrome(options=options)
+
+with sqlite3.connect('problems.db') as conn:
+    cursor = conn.cursor()  
+    cursor.execute('SELECT * from problems')
+    problem_list = cursor.fetchall()
+
+conn = sqlite3.connect('solutions.db')
+cursor = conn.cursor()
+
+# Create a table
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS solutions (
+    srno INT,
+    prob_srno INT,
+    coder_href TEXT,
+    rel_href TEXT,
+    code_language TEXT,
+    code_length TEXT,
+    code TEXT
+)
+''')
+
+URL = {
+    'leetcode': f'https://leetcode.com',
+    'problemset': f'https://leetcode.com/problemset',
+    'problems': f'https://leetcode.com/problems'
+}
+
+solu_srno = 1
+prob_srno = 1
+for prob in problem_list[:]:
+    # print('\nNew Problem', prob)
+    prob_no, prob_title, prob_href = prob
+    driver.get(f'{URL["problems"]}/{prob_href}/solutions')
+    time.sleep(5)
+
+    soup = BeautifulSoup(driver.page_source, 'html.parser')
+
+    solutions_divs = soup.find_all('div', class_=['flex', 'w-full', 'flex-col'])
+    try: solutions_divs = [div for div in solutions_divs if set(div['class']) == {'flex', 'w-full', 'flex-col'}][0].find_all('div', recursive=False)
+    except: continue
+
+    for solu in solutions_divs:
+        # print('\nok', solu)
+        if 'group/ads' in solu['class']: continue
+        try:
+            coder_href, _, solu_href = map(lambda a: a.get('href'), solu.find_all('a'))
+
+            # print(f'{URL["leetcode"]}{solu_href}')
+            driver.get(f'{URL["leetcode"]}{solu_href}')
+            time.sleep(5)
+
+            soup = BeautifulSoup(driver.page_source, 'html.parser')
+
+            max_ = 0
+            solu_code = None
+            for code in soup.find_all('code'):
+                if len(code.text) > max_:
+                    solu_code = code
+                    max_ = len(code.text)
+            
+            solu_code_language = solu_code['class'][0].replace('language-', '')
+            solu_code_length = max_
+            solu_code = solu_code.text
+        except: continue
+        else:
+            cursor.execute('INSERT INTO solutions (srno, prob_srno, coder_href, rel_href, code_language, code_length, code) VALUES (?, ?, ?, ?, ?, ?, ?)', (solu_srno, prob_srno, prob_href, coder_href, solu_href, solu_code_language, solu_code))
+            conn.commit()
+
+            solu_srno += 1
+
+    
+    prob_srno += 1
+
+conn.close()
